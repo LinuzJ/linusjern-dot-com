8e1e65edac86a3ea038e5e46dfa96ce9940dfaf7
LinuzJ <linusjern00@gmail.com>
2023-07-24 22:17:24 +0300

    more seo

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -1,16 +1,33 @@
 import Head from "next/head"
 import { site } from "~/pages/_app"
 
+const structData = {
+  "@context": "https://linusjern.com",
+  "@type": "Article",
+  headline: "Linus Jern",
+  description: "Hello there! I'm Linus Jern.",
+  author: [
+    {
+      "@type": "Person",
+      name: "Linus Jern",
+    },
+  ],
+}
+
 export default function MainHead() {
   return (
     <Head>
-      <meta name='description' content="Hello there! I'm Linus Jern." />
       <meta
         property='og:image'
         content={"https://linusjern.com/images/me.jpg"}
       />
       <meta name='og:title' content={site} />
       <meta name='twitter:card' content='summary' />
+      <script
+        key='structured-1'
+        type='application/ld+json'
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(structData) }}
+      />
     </Head>
   )
 }
