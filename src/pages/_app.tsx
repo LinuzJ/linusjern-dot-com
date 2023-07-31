efbd6de143eb2f85a300ef7bbfd13c8738d19c36
LinuzJ <linus.jern@smartly.io>
2023-07-31 13:24:13 +0300

    Improve seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,46 +1,32 @@
-import { type AppType } from "next/dist/shared/lib/utils"
-import Head from "next/head"
-import Script from "next/script"
-import "~/styles/globals.css"
-import * as gtag from "~/misc/gtag"
+import { type AppType } from "next/dist/shared/lib/utils";
+import Head from "next/head";
+import Script from "next/script";
+import "~/styles/globals.css";
+import { GA_TRACKING_ID } from "~/misc/gtag";
 
-export const site = "Linus Jern"
+export const site = "Linus Jern";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <>
       <Script
-        strategy='afterInteractive'
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        id='gtag-init'
-        strategy='afterInteractive'
-        dangerouslySetInnerHTML={{
-          __html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${gtag.GA_TRACKING_ID}', {
-          page_path: window.location.pathname,
-        });
-      `,
-        }}
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
       />
       <Head>
         <title>Linus Jern</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
         <link
-          rel='apple-touch-icon'
-          sizes='180x180'
-          href='/apple-touch-icon.png'
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
         />
-        <meta name='apple-mobile-web-app-title' content={site} />
-        <meta name='application-name' content={site} />
+        <meta name="apple-mobile-web-app-title" content={site} />
+        <meta name="application-name" content={site} />
       </Head>
       <Component {...pageProps} />
     </>
-  )
-}
+  );
+};
 
-export default MyApp
+export default MyApp;
