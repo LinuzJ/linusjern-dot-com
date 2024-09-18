8326393e2ac3a47217d25b3d5aff0891cdc5d625
LinuzJ <linusjern00@gmail.com>
2024-09-18 16:51:19 +0300

    update

--- src/components/mdx-remote.js
+++ src/components/mdx-remote.js
@@ -1,24 +1,35 @@
-import { MDXRemote } from 'next-mdx-remote/rsc'
-import Image from 'next/image'
+import { MDXRemote } from 'next-mdx-remote/rsc';
+import Image from 'next/image';
 
 const components = {
   h1: (props) => (
-    <h1 {...props} className="text-2xl">
+    <h1 {...props} className='text-2xl'>
       {props.children}
     </h1>
   ),
+  h2: (props) => (
+    <h2 {...props} className='text-xl'>
+      {props.children}
+    </h2>
+  ),
   a: (props) => (
-    <a {...props} className="text-link-color hover:underline" href={props.href} target="_blank" rel="noopener noreferrer">
+    <a
+      {...props}
+      className='text-link-color hover:underline'
+      href={props.href}
+      target='_blank'
+      rel='noopener noreferrer'
+    >
       {props.children}
     </a>
   ),
   p: (props) => (
-    <p {...props} className="mb-[1em] mt-[1em] ">
+    <p {...props} className='mb-[1em] mt-[1em] '>
       {props.children}
     </p>
   ),
-  Image: (props) => <Image {...props} />
-}
+  Image: (props) => <Image {...props} />,
+};
 
 export function CustomMDX(props) {
   return (
@@ -26,5 +37,5 @@ export function CustomMDX(props) {
       {...props}
       components={{ ...components, ...(props.components || {}) }}
     />
-  )
+  );
 }
